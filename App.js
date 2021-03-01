bdec46430e874ab35cc3f25304bdb221e1313fc2
elir-mahad <mmahad280@gmail.com>
2021-03-01 09:39:26 -0500

    added the react navigation old version as a dependency and added another dependecy called whatg. Added these two dependencies because the tutorial is from 2018 and the teaacher said thatby addding these dependencies we can make sure that the app doesn't break because of all the new versions and updates.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text> The Cart </Text>
+			<Text> The Cart Here right now </Text>
 			<StatusBar style="auto" />
 		</View>
 	);
