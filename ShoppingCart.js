ba7d5f7d20a0fe7417dabd7174568759494a5c5b
elir-mahad <mmahad280@gmail.com>
2021-03-01 09:44:08 -0500

    added the shopping cart componenet and imported to the app.js

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,21 +1,19 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import ShoppingCart from "./ShoppingCart";
 
-export default function App() {
+const ShoppingCart = () => {
 	return (
 		<View style={styles.container}>
-			<StatusBar style="auto" />
-			<ShoppingCart />
+			<Text>Shopping Cart</Text>
 		</View>
 	);
-}
+};
+
+export default ShoppingCart;
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center"
 	}
