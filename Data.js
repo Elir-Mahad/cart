e34821f3677c04197e488b3a0fedca2f7cd537b5
elir-mahad <mmahad280@gmail.com>
2021-03-01 09:25:46 -0500

    added the data file that contains two objects book and electronices. each object has 3 items

---
+++ Data.js
@@ -0,0 +1,35 @@
+export const electronics = [
+	{
+		id: 1,
+		name: "fifa 19",
+		price: 49.99
+	},
+	{
+		id: 2,
+		name: "Amazon eCHO",
+		price: 199
+	},
+	{
+		id: 3,
+		name: "Bose headphones",
+		price: 300
+	}
+];
+
+export const books = [
+	{
+		id: 4,
+		name: "How to kill a mocking bird",
+		price: 49.99
+	},
+	{
+		id: 5,
+		name: "Red white and black",
+		price: 59
+	},
+	{
+		id: 6,
+		name: "Scenes of subjection",
+		price: 29.99
+	}
+];
