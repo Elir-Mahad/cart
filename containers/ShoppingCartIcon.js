85dadaa2fe85ed790343775f292d8762a96db4ff
elir-mahad <mmahad280@gmail.com>
2021-03-01 11:13:36 -0500

    added some styles for the icon and the item count text

--- containers/ShoppingCartIcon.js
+++ containers/ShoppingCartIcon.js
@@ -5,8 +5,10 @@ import Icon from "react-native-vector-icons/Ionicons";
 const ShoppingCartIcon = () => {
 	return (
 		<View style={styles.container}>
+			<View style={styles.itemcount}>
+				<Text>0</Text>
+			</View>
 			<Icon name="ios-cart" size={30} />
-			<Text>0</Text>
 		</View>
 	);
 };
@@ -16,5 +18,16 @@ export default ShoppingCartIcon;
 const styles = StyleSheet.create({
 	container: {
 		padding: 5
+	},
+	itemcount: {
+		position: "absolute",
+		height: 30,
+		width: 30,
+		borderRadius: 15,
+		backgroundColor: "lightblue",
+		right: 35,
+		bottom: 15,
+		alignItems: "center",
+		justifyContent: "center"
 	}
 });
